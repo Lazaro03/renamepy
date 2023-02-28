81e6fac256019c6122a97ca2bf13a60b2183d987
Lazaro03 <95556479+Lazaro03@users.noreply.github.com>
2023-02-28 14:45:47 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "18517936")
+API_ID = os.environ.get("API_ID", "11405596")
 
-API_HASH = os.environ.get("API_HASH", "552ce63ba82ea7a633b039ee0dc87790")
+API_HASH = os.environ.get("API_HASH", "39ad6135ac9b75a47f2d1804c092c8f8)
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5535408997:AAEmnT-ZdCsy43HsxwRJe0It0hzSC0e7zO8") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6200989786:AAFnEUzr9GL_6yLzUfHjazYOnilJVYbdarM") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "k2m_movies_series") 
 
@@ -18,6 +18,6 @@ FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://graph.org/file/1f04a86d2526b25b34d55.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1753576629').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '878626783').split()]
 
 PORT = os.environ.get("PORT", "8080")
